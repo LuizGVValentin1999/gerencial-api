9428ea0fb8fc14a71dde594c4b7b1810c7b14034
LuizGVValentin1999 <luiz.gustavo1999@hotmail.com>
2022-06-29 23:53:07 -0300

    liberando o kernel do sanctum e criando teste
    meio que  o comit é auto explicativo

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+Route::get('criarTokenTeste',function(){
+    $user = User::find(1);
+    $token = $user->createToken('login');
+    dd($token->plainTextToken);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
