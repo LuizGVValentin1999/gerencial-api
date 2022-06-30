9a266ce2a8cc073d3869876eb38e0fe84b60fe83
LuizGVValentin1999 <luiz.gustavo1999@hotmail.com>
2022-06-29 23:43:50 -0300

    configurando o banco de dados (local) e criando um usuario
    criando as tabelas padores do laravel
    php artisan migrate
    
    rodando o seed do db para criar o usuario
    php artisan db:seed (isso acabei de rodar não mudou nada nos arquivos )

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\User;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,6 +17,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create( [
+            'name'=>'Guga',
+            'email'=>'luiz.gustavo1999@hotmail.com',
+            'password'=>bcrypt("123123"), 
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
